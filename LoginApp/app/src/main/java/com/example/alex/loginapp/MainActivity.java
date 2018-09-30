2f40c332fc7910fb7969f893e4fa2942076a84b9
Preda Alexandru <alexpreda95@yahoo.com>
2018-09-30 11:55:36 +0300

    Add LoginApp Project

---
+++ LoginApp/app/src/main/java/com/example/alex/loginapp/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.alex.loginapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonSignup;
+
+    private TextView textViewSignin;
+
+    private ProgressDialog progressDialog;
+
+
+    //definim obiectul firebase
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initializam obiectul firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+            finish();
+
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+        }
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignin = (TextView) findViewById(R.id.textViewSignin);
+
+        buttonSignup = (Button) findViewById(R.id.buttonSignup);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonSignup.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+
+        //preluam email + parola din edit text
+        String email = editTextEmail.getText().toString().trim();
+        String password  = editTextPassword.getText().toString().trim();
+
+        //verificam daca email + parola sunt necompletate
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // daca cele doua campuri sunt completate afisam un mesaj de asteptare
+        progressDialog.setMessage("Registering Please Wait...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        }else{
+                            Toast.makeText(MainActivity.this,"Registration Error",Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view == buttonSignup){
+            registerUser();
+        }
+
+        if(view == textViewSignin){
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+    }
+}
